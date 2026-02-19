396166621de98cd41132c0be835e35ae05d85dfa
Koushik Nakka <venkatakoushik777@gmail.com>
2026-02-18 21:56:36 -0500

    fix(apollo): use scoped graphql uri for msw

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: '/graphql',
+    uri: `${import.meta.env.BASE_URL}graphql`,
     cache: new InMemoryCache(),
 });
